b65d048b0a70a97b37d732545cfeb44b9d3f2642
Lucas <Lucas@ASUS>
2024-11-21 22:13:13 -0300

    Actualizacion del archivo main. Ahora esta completo el programa

--- Boost_PWM_ADC_USB_v1.0/Core/Src/main.c
+++ Boost_PWM_ADC_USB_v1.0/Core/Src/main.c
@@ -22,7 +22,11 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "stdbool.h"
+#include "math.h"
+#include "stdio.h"
+#include <string.h> // Necesario para memcpy
+#include "usbd_cdc_if.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -61,10 +65,83 @@ static void MX_TIM4_Init(void);
 static void MX_TIM8_Init(void);
 /* USER CODE BEGIN PFP */
 
+void DMA_Transfer(uint16_t *source, uint16_t *destination, uint16_t size);
+void convert_Uint16_to_Uint8(uint16_t *srcArray, size_t srcLength, uint8_t *destArray);
+
+
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+#define ITM_Port32(n) (*((volatile unsigned long *) (0xE0000000+4*n)))
+
+// Variables del ADC
+#define CANALES_ADC 8									//Cantidad de canales que muestrea el ADC
+#define BUFFER_CANALES_ADC 10							//Cantidad de mediciones de cada canal que se guardara antes de enviarlos
+#define ADC_BUFFER_SIZE CANALES_ADC*BUFFER_CANALES_ADC  //Size del buffer donde el DMA guardara las mediciones del ADC
+#define ADC_BUFFER_HALF_SIZE ADC_BUFFER_SIZE/2
+
+uint16_t adc1_array[ADC_BUFFER_SIZE]; 					//Array donde el DMA guarda las conversiones del ADC1 [Io, LEM, i30, Vo, IL, Vext, Pote, Vin]
+uint16_t tx_buffer1[ADC_BUFFER_HALF_SIZE];				//Array donde se guarda la primera mitad de los datos muestreados por el ADC (adc1_array)
+uint16_t tx_buffer2[ADC_BUFFER_HALF_SIZE];				//Array donde se guarda la segunda mitad de los datos muestreados por el ADC (adc1_array)
+
+//Array que se va a enviar por USB. Para evitar que el ADC sobreescriba los datos del array antes de que estos
+//sean envaidos, se utiliza una tecnica que consiste en:
+//	1. Almacenar los datos del ADC en un Array (adc1_array)
+//  2. Cuando el DMA lleno la mitad del array (adc1_array) mueve estos datos al array tx_buffer1
+//  3. Como el array tx_buffer1 es de 16 bits, se convierten cada uno de sus valores a datos de 8 bits que se almacenan en el array "tx_buffer_final"
+//     Por eso, el tamaño del array "tx_buffer_final" es el doble del array "tx_buffer1". Una vez convertido, el array "tx_buffer_final" es enviado por USB.
+//  4. Mientras el punto 3 se ejecuta, el DMA continua llenando la segunda mitad del array "adc1_array", una vez terminado, se repite el punto 3 pero con el array "tx_buffer2"
+
+uint8_t tx_buffer_final[ADC_BUFFER_SIZE+2]; 			//Tamaño del encabezado (2 bytes) + tamaño de los datos convertidos (2 * número de elementos en srcArray)
+
+uint16_t duty_pwm = 0;
+uint16_t trigger = 0;
+
+uint16_t cont = 0;
+
+//--Variables para la banda muerta
+uint16_t x_prev = 0;
+float deadBand = 0.05;
+
+float duty= 0;
+
+float bandaMuerta(float x) {		//Banda muerta para el potenciometro
+
+	if (fabs(x - x_prev) < deadBand) {
+		x = x_prev;
+	} else {
+		x_prev = x;
+	}
+
+	if (x < 0.1){
+		return 0.1;
+	}
+	else if (x > 0.9){
+		return 0.9;
+	}
+	else{
+		return x;
+	}
+}
+
+void set_period(float duty){
+
+	duty_pwm = duty * (htim4.Init.Period + 1);
+
+	if (duty > 0.5){
+		trigger = (duty*0.5) * (htim8.Init.Period + 1);
+	}
+	else{
+		trigger = ((1-duty)*0.5 + duty) * (htim8.Init.Period + 1);
+	}
+
+	trigger = duty_pwm;
+
+	__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_1, duty_pwm);
+	__HAL_TIM_SET_COMPARE(&htim8,TIM_CHANNEL_3, trigger);
+}
 
 /* USER CODE END 0 */
 
@@ -104,12 +181,47 @@ int main(void)
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
 
+	DBGMCU->APB1FZ |= (DBGMCU_APB1_FZ_DBG_TIM4_STOP);//Detiene el clock del APB1 en Debug Mode
+	DBGMCU->APB2FZ |= (DBGMCU_APB2_FZ_DBG_TIM8_STOP);//Detiene el clock del APB2 en Debug Mode
+
+	__HAL_TIM_SET_COUNTER(&htim4, 0);//Reseteo los contadores de ambos timers para que cuenten en paralelo
+	__HAL_TIM_SET_COUNTER(&htim8, 0);
+
+	if (HAL_ADC_Start_DMA(&hadc1, (uint32_t*) &adc1_array, (sizeof(adc1_array) / sizeof(adc1_array[0]))) != HAL_OK) {//Inicio el DMA para enviar los datos del ADC a la variable "adc1_array" -> Iniciarlo antes que los Timers!!!
+		Error_Handler();
+	}
+
+
+	if (HAL_TIM_PWM_Start_IT(&htim4, TIM_CHANNEL_1) != HAL_OK) {//Inicio el CH1 del Timer 4. Salida del  PWM
+		Error_Handler();
+	}
+
+	if (HAL_TIM_PWM_Start_IT(&htim8, TIM_CHANNEL_3) != HAL_OK) {//Inicio el CH3 del Timer 8. Salida del  PWM
+		Error_Handler();
+	}
+
+	__HAL_TIM_SET_COMPARE(&htim4,TIM_CHANNEL_1, 0.5*(htim4.Init.Period + 1));
+	__HAL_TIM_SET_COMPARE(&htim8,TIM_CHANNEL_3, 0.5*(htim8.Init.Period + 1));
+
+	HAL_GPIO_WritePin(Rele_1_GPIO_Port, Rele_1_Pin, 1);
+	HAL_GPIO_WritePin(Rele_2_GPIO_Port, Rele_2_Pin, 1);
+	HAL_GPIO_WritePin(Rele_3_GPIO_Port, Rele_3_Pin, 1);
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+	  HAL_Delay(500);
+
+	  duty = bandaMuerta(adc1_array[6]/4096.0);
+
+
+
+	  set_period(duty);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -499,6 +611,83 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
+
+
+void convert_Uint16_to_Uint8(uint16_t *srcArray, size_t srcLength, uint8_t *destArray){
+
+    // Agregar encabezado
+    destArray[0] = 0xAA; // Primer byte del encabezado
+    destArray[1] = 0x55; // Segundo byte del encabezado
+
+    // Convertir uint16_t a uint8_t y agregar al array de destino
+    for (size_t i = 0; i < srcLength; i++) {
+        destArray[2 + 2 * i] = srcArray[i] & 0xFF;       	  // Parte baja del uint16_t
+        destArray[2 + 2 * i + 1] = (srcArray[i] >> 8) & 0xFF; // Parte alta del uint16_t
+
+    }
+
+
+    ITM_Port32(2) = 1;
+    CDC_Transmit_FS((uint8_t *) tx_buffer_final, sizeof(tx_buffer_final));	//Envia por USB los datos
+    ITM_Port32(2) = 0;
+}
+
+
+void DMA_Transfer(uint16_t *source, uint16_t *destination, uint16_t size){
+
+    ITM_Port32(20) = 0;
+    // Inicia la transferencia DMA
+    if (HAL_DMA_Start(&hdma_memtomem_dma2_stream1, (uint32_t)source, (uint32_t)destination, size) != HAL_OK) {
+        Error_Handler();         // Error de inicio
+    }
+
+    HAL_DMA_PollForTransfer(&hdma_memtomem_dma2_stream1, HAL_DMA_FULL_TRANSFER, 1);
+    ITM_Port32(20) = 1;
+
+}
+
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {									  	//Interrupcion que indica el fin de la conversion
+
+	if (hadc->DMA_Handle == &hdma_adc1) {
+		HAL_GPIO_WritePin(LED_ROJO_GPIO_Port, LED_ROJO_Pin, 1);
+		DMA_Transfer(&adc1_array[ADC_BUFFER_HALF_SIZE], &tx_buffer2[0], ADC_BUFFER_HALF_SIZE); 	//Transfiere la segunda mitad de los datos al registro tx_buffer2
+		convert_Uint16_to_Uint8(tx_buffer2, ADC_BUFFER_HALF_SIZE, tx_buffer_final);
+	}
+
+}
+
+
+void HAL_ADC_ConvHalfCpltCallback(ADC_HandleTypeDef *hadc) {									//Interrupcion cuando el DMA lleno la mitad del buffer
+
+	if (hadc->DMA_Handle == &hdma_adc1) {
+		HAL_GPIO_WritePin(LED_ROJO_GPIO_Port, LED_ROJO_Pin, 1);
+		DMA_Transfer(&adc1_array[0], &tx_buffer1[0], ADC_BUFFER_HALF_SIZE);						//Transfiere la primer mitad de los datos al registro tx_buffer1
+		convert_Uint16_to_Uint8(tx_buffer1, ADC_BUFFER_HALF_SIZE, tx_buffer_final);
+	}
+
+}
+
+
+
+
+//Funcion Callback llamada por las interrupciones de los timers. Deben estar habilitadas en la configuracion
+// Ingresa con el flanco descendente del PWM
+void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
+
+	if (htim->Instance == TIM8) {			//TRGO
+		ITM_Port32(30) = 1;
+		HAL_GPIO_TogglePin(LED_AZUL_GPIO_Port, LED_AZUL_Pin);	//PD15
+//
+		HAL_GPIO_WritePin(LED_ROJO_GPIO_Port, LED_ROJO_Pin, 0); //PD14
+	}
+
+	if (htim->Instance == TIM4) {			//PWM -
+		ITM_Port32(30) = 2;
+		HAL_GPIO_TogglePin(LED_VERDE_GPIO_Port, LED_VERDE_Pin); //PD12
+
+	}
+}
 /* USER CODE END 4 */
 
 /**
